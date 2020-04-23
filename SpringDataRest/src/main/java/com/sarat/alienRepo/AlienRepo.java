1b95a982c804df738a8c4259755adf976f3e040b
Subha750 <64191800+Subha750@users.noreply.github.com>
2020-04-23 18:13:39 +0530

    AlienRepooneslas
    no need

--- SpringDataRest/src/main/java/com/sarat/alienRepo/AlienRepo.java
+++ SpringDataRest/src/main/java/com/sarat/alienRepo/AlienRepo.java
@@ -5,7 +5,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.sarat.model.Alien;
 @RepositoryRestResource(collectionResourceRel="aliens",path="aliens")//in that application we are not using controler 
-///only use that above Annotation it will handle and mapped every request
+//only use that above Annotation it will handle and mapped every request
 public interface AlienRepo extends JpaRepository<Alien,Integer>
 {
 
